082e925a1e4b35d7fffa749c68ed8dc5a06fb1d3
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-25 01:44:43 +0530

    [Dipti] added code for displaying Week days using java script

---
+++ Case/DisplayWeekDay.js
@@ -0,0 +1,46 @@
+//Read the number and display the corresponding day
+const prompt = require("prompt-sync")();
+const day=prompt("Enter the day from 1 to 7: ");
+switch(day)
+{
+    case "1":
+    {
+        console.log("The day is :Sunday");
+        break;
+    }
+    case "2":
+    {
+        console.log("The day is :Monday");
+        break;
+    }
+    case "3":
+    {
+        console.log("The day is :Tuesday");
+        break;
+    }
+    case "4":
+    {
+        console.log("The day is :Wednesday");
+        break;
+    }
+    case "5":
+    {
+        console.log("The day is :Thursday");
+        break;
+    }
+    case "6":
+    {
+        console.log("The day is :Friday");
+        break;
+    }
+    case "6":
+    {
+        console.log("The day is :Saturday");
+        break;
+    }
+    default:
+    {
+        console.log("Invalid day!");
+        break;
+    }
+}
